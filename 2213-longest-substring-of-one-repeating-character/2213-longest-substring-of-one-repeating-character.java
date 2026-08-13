33cbac2e7f229f8b5822859063676c1a1e0bee1a
Himanshu Yadav <6604711@rungta.org>
2026-08-13 23:11:36 +0530

    Time: 111 ms (43.75%), Space: 157 MB (17.19%) - LeetHub

--- 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.java
+++ 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.java
@@ -1,30 +1,95 @@
 class Solution {
 
     class Node {
-        int leftChar;
-        int rightChar;
-        int leftLen;
-        int rightLen;
-        int maxLen;
-        int len;
-
-        Node() {}
+        char leftChar, rightChar;
+        int prefix, suffix, best, len;
 
         Node(char c) {
-            leftChar = c;
-            rightChar = c;
-            leftLen = rightLen = maxLen = len = 1;
+            leftChar = rightChar = c;
+            prefix = suffix = best = len = 1;
         }
+
+        Node() {}
     }
 
     Node[] tree;
     String s;
 
-    public int[] longestRepeating(String s, String queryCharacters, int[] queryIndices) {
+    void build(int node, int l, int r) {
+        if (l == r) {
+            tree[node] = new Node(s.charAt(l));
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        build(node * 2, l, mid);
+        build(node * 2 + 1, mid + 1, r);
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    Node merge(Node a, Node b) {
+
+        Node res = new Node();
+
+        res.len = a.len + b.len;
+        res.leftChar = a.leftChar;
+        res.rightChar = b.rightChar;
+
+        res.prefix = a.prefix;
+        res.suffix = b.suffix;
+        res.best = Math.max(a.best, b.best);
+
+        // If boundary characters are same,
+        // we can combine suffix of left + prefix of right
+        if (a.rightChar == b.leftChar) {
+
+            if (a.prefix == a.len) {
+                res.prefix = a.len + b.prefix;
+            }
+
+            if (b.suffix == b.len) {
+                res.suffix = b.len + a.suffix;
+            }
+
+            res.best = Math.max(
+                res.best,
+                a.suffix + b.prefix
+            );
+        }
+
+        return res;
+    }
+
+    void update(int node, int l, int r, int idx, char c) {
+
+        if (l == r) {
+            tree[node] = new Node(c);
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        if (idx <= mid) {
+            update(node * 2, l, mid, idx, c);
+        } else {
+            update(node * 2 + 1, mid + 1, r, idx, c);
+        }
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    public int[] longestRepeating(
+        String s,
+        String queryCharacters,
+        int[] queryIndices
+    ) {
 
         this.s = s;
 
         int n = s.length();
+
         tree = new Node[4 * n];
 
         build(1, 0, n - 1);
@@ -34,91 +99,14 @@ class Solution {
 
         for (int i = 0; i < k; i++) {
 
-            int index = queryIndices[i];
-            char ch = queryCharacters.charAt(i);
+            int idx = queryIndices[i];
+            char c = queryCharacters.charAt(i);
 
-            update(1, 0, n - 1, index, ch);
+            update(1, 0, n - 1, idx, c);
 
-            ans[i] = tree[1].maxLen;
+            ans[i] = tree[1].best;
         }
 
         return ans;
-    }
-
-    // Build segment tree
-    void build(int node, int start, int end) {
-
-        if (start == end) {
-            tree[node] = new Node(s.charAt(start));
-            return;
-        }
-
-        int mid = (start + end) / 2;
-
-        build(node * 2, start, mid);
-        build(node * 2 + 1, mid + 1, end);
-
-        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
-    }
-
-    // Update one character
-    void update(int node, int start, int end, int index, char ch) {
-
-        if (start == end) {
-            tree[node] = new Node(ch);
-            return;
-        }
-
-        int mid = (start + end) / 2;
-
-        if (index <= mid) {
-            update(node * 2, start, mid, index, ch);
-        } else {
-            update(node * 2 + 1, mid + 1, end, index, ch);
-        }
-
-        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
-    }
-
-    // Merge two segments
-    Node merge(Node left, Node right) {
-
-        Node res = new Node();
-
-        res.len = left.len + right.len;
-
-        res.leftChar = left.leftChar;
-        res.rightChar = right.rightChar;
-
-        // Left prefix
-        res.leftLen = left.leftLen;
-
-        if (left.leftLen == left.len &&
-            left.rightChar == right.leftChar) {
-
-            res.leftLen = left.len + right.leftLen;
-        }
-
-        // Right suffix
-        res.rightLen = right.rightLen;
-
-        if (right.rightLen == right.len &&
-            left.rightChar == right.leftChar) {
-
-            res.rightLen = right.len + left.rightLen;
-        }
-
-        // Maximum inside either segment
-        res.maxLen = Math.max(left.maxLen, right.maxLen);
-
-        // Maximum crossing the middle
-        if (left.rightChar == right.leftChar) {
-            res.maxLen = Math.max(
-                res.maxLen,
-                left.rightLen + right.leftLen
-            );
-        }
-
-        return res;
     }
 }
